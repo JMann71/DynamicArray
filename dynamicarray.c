57896c5997927080262920cccce8d2297f53ddc7
JManness71 <74986019+JManness71@users.noreply.github.com>
2022-10-19 12:56:20 -0400

    Created Resize Function

--- dynamicarray.c
+++ dynamicarray.c
@@ -10,6 +10,8 @@ int reserved_size;
     @desc This function creates an array of integers based on the size passed in
     @author Jarod Manness
     @params {size} number of elements to initialize
+    @params {max} member variable holding the max capacity of the array
+    @params {reserved} member variable holding the number of used elements
     @return {vector} a pointer to the created array
 */
 int* CreateVector(int size, int *max, int *reserved){
@@ -18,6 +20,32 @@ int* CreateVector(int size, int *max, int *reserved){
     *max = size;
     *reserved = size;
     return vector;
+}
+
+/*
+    @desc This function creates a new array of a given size, copies data from an input array into it, then reassigns the pointer to the new array
+    @author Jarod Manness
+    @params {array} Input array to be copied to the new larger array
+    @params {size} The size of the new array
+    @params {max} member variable holding the max capacity of the array
+    @params {reserved} member variable holding the number of used elements
+*/
+void Resize(int *array, int size, int *max, int *reserved) {
+    // New Array must be larger than old
+    if(size < *max) {
+        return;
+    }
+    int *newVector;
+    newVector = (int *)malloc(size * sizeof(int)); // Creates a new array of the requested size
+    for(int i = 0; i < *reserved; i++){
+        newVector[i] = array[i];
+    }
+    free(array);
+
+    array = newVector;
+    newVector = NULL;
+    *max = size;
+    return;
 }
 
 void main(){
